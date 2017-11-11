77af422d87209ae8dbc7f8cf11f50e151d8175e4
Hyunkyu Michael Lee <hkl0902@gmail.com>
2017-11-11 15:09:43 -0800

    Add simple tests

--- tests/graph_tests.py
+++ tests/graph_tests.py
@@ -1,7 +1,20 @@
 from Graph import *
 import unittest
+import string
  
 class TestGraph(unittest.TestCase):
+
+    def test_node_initialization(self):
+        for n, i in zip(list(string.ascii_letters), range(len(string.ascii_letters))):
+            node = Node(n, i)
+            self.assertTrue(node.name  == n)
+            self.assertTrue(node.index == i)
+
+    def test_graph_initialization(self):
+        G = Graph([], {}, 0)
+        self.assertTrue(len(G.V)     == 0)
+        self.assertTrue(len(G.E)     == 0)
+        self.assertTrue(G.nextIndex  == 0)
 
     def test_add_edge(self):
         pass
@@ -24,4 +37,5 @@ class TestGraph(unittest.TestCase):
     def test_find_min_path(self):
         pass
 
-        
+if __name__ == '__main__':
+    unittest.main()
