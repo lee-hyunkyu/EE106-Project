ed225dc8e9a299ea95cec5b056c40429739cc916
Hyunkyu Michael Lee <hkl0902@gmail.com>
2017-11-12 20:00:45 -0800

    Modify graph representation.
    This is for easier use and faster lookup of vertices/neighbors

--- src/Graph.py
+++ src/Graph.py
@@ -27,9 +27,10 @@ class Edge:
         return self._node2s
 
 class Graph: 
-    def __init__(self, V=[], E={}, nextIndex=0):
+    def __init__(self, V={}, E={}, v_names={}, nextIndex=0):
         self._nextIndex = nextIndex
-        self._V = V # Dictionary of {Index:Node}
+        self._V        = V          # Dictionary of {Index:Node} 
+        self._V_names  = v_names    # Dictionary of {Name :Node}
 
         '''
         if (u, v) is an edge
@@ -49,17 +50,24 @@ class Graph:
     def E(self):
         return self._E
 
-    def add_node(self, node):
-        self.V[node.index]  = node
-        self.nextIndex      += 1
+    @property
+    def V_names(self):
+        return self._V_names
+
+    def add_node(self, name):
+        node                     = Node(name, self.nextIndex) 
+        self._V[node.index]      = node
+        self._V_names[node.name] = node
+        self._nextIndex          += 1
+        return node
 
     def add_edge(self, node1, node2, length):
-        self.E[node1.index][node2.index] = Edge(length, node1, node2)
-        self.E[node2.index][node1.index] = Edge(length, node2, node1)
+        self._E[node1.index][node2.index] = Edge(length, node1, node2)
+        self._E[node2.index][node1.index] = Edge(length, node2, node1)
 
     def remove_edge(self, node1, node2):
-        self.E[node1.index][node2.index] = Edge(0, node1, node2)
-        self.E[node2.index][node1.index] = Edge(0, node2, node1)
+        self._E[node1.index][node2.index] = Edge(0, node1, node2)
+        self._E[node2.index][node1.index] = Edge(0, node2, node1)
 
     def get_neighbors(self, node):
         neighbors = []
@@ -70,6 +78,9 @@ class Graph:
 
     def get_edge(self, node1, node2):
         return self.E[node1.index][node2.index]
+
+    def get_node(self, name):
+        return self._V_names.get(name)
 
     def breadth_first_search(self):
         pass
