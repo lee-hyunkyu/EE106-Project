2a7bb0d4828007a78f3fd46d4676e3182726f740
Hyunkyu Michael Lee <hkl0902@gmail.com>
2017-11-29 21:10:28 -0800

    Adds more test for adding neighbors

--- tests/graph_tests.py
+++ tests/graph_tests.py
@@ -47,20 +47,225 @@ class TestGraph(unittest.TestCase):
         self.assertFalse(b.is_end)
         self.assertFalse(c.is_end)
 
-
     def test_graph_initialization(self):
-        self.graph = Graph()
-        self.assertTrue(len(self.graph.V)     == 0)
-        self.assertTrue(self.next_index       == 0)
+        graph = Graph()
+        self.assertTrue(len(graph.V)     == 0)
+        self.assertTrue(graph.next_index == 0)
 
     def test_add_node(self):
-        pass
+        g = Graph()
+        n1 = g.add_node()
+        n2 = g.add_node()
+        n3 = g.add_node()
+        n4 = g.add_node()
+
+        self.assertTrue(len(g.V) == 4)
+
+        for i in range(4):
+            self.assertTrue(i in g.V)
+
+        nodes = [n1, n2, n3, n4]
+
+        for i, n in zip(range(4), nodes):
+            self.assertTrue(g.V[i] is n)
+            self.assertTrue(g.V[i] == n)
+
+        self.assertTrue(g.next_index == 4)
 
     def test_get_node(self):
-        pass
+        g = Graph()
+        n1 = g.add_node()
+        n2 = g.add_node()
+        n3 = g.add_node()
+        n4 = g.add_node()
 
-    def test_get_neighbors(self):
-        pass
+        nodes = [n1, n2, n3, n4]
+        for i, n in zip(range(4), nodes):
+            self.assertTrue(g.get_node(i) == n)
+            self.assertTrue(g.get_node(i) is n)
+
+    def test_add_neighbor(self):
+        g = Graph()
+        n1 = g.add_node()
+        n2 = g.add_node()
+        n3 = g.add_node()
+        n4 = g.add_node()
+        n5 = g.add_node()
+        n6 = g.add_node()
+        n7 = g.add_node()
+        n8 = g.add_node()
+        n9 = g.add_node()
+
+        # Imagine a configuration similar to a telephone numpad
+        north = Direction.NORTH()
+        east  = Direction.EAST()
+        south = Direction.SOUTH()
+        west  = Direction.WEST()
+
+
+        self.assertTrue(n1.add_neighbor(n2, east))
+        self.assertTrue(n1.add_neighbor(n4, south))
+        self.assertTrue(n2.add_neighbor(n1, west))
+        self.assertTrue(n2.add_neighbor(n3, east))
+        self.assertTrue(n2.add_neighbor(n5, south))
+        self.assertTrue(n3.add_neighbor(n2, west))
+        self.assertTrue(n3.add_neighbor(n6, south))
+
+        self.assertTrue(n4.add_neighbor(n1, north))
+        self.assertTrue(n4.add_neighbor(n5, east))
+        self.assertTrue(n4.add_neighbor(n7, south))
+        self.assertTrue(n5.add_neighbor(n4, west))
+        self.assertTrue(n5.add_neighbor(n2, north))
+        self.assertTrue(n5.add_neighbor(n6, east))
+        self.assertTrue(n5.add_neighbor(n8, south))
+        self.assertTrue(n6.add_neighbor(n3, north))
+        self.assertTrue(n6.add_neighbor(n5, west))
+        self.assertTrue(n6.add_neighbor(n9, south))
+
+
+        self.assertTrue(n7.add_neighbor(n4, north))
+        self.assertTrue(n7.add_neighbor(n8, east))
+        self.assertTrue(n8.add_neighbor(n7, west))
+        self.assertTrue(n8.add_neighbor(n5, north))
+        self.assertTrue(n8.add_neighbor(n9, east))
+        self.assertTrue(n9.add_neighbor(n8, west))
+        self.assertTrue(n9.add_neighbor(n6, north))
+
+        self.assertTrue(len(n1.neighbors) == 2)
+        self.assertTrue(len(n2.neighbors) == 3)
+        self.assertTrue(len(n3.neighbors) == 2)
+        self.assertTrue(len(n4.neighbors) == 3)
+        self.assertTrue(len(n5.neighbors) == 4)
+        self.assertTrue(len(n6.neighbors) == 3)
+        self.assertTrue(len(n7.neighbors) == 2)
+        self.assertTrue(len(n8.neighbors) == 3)
+        self.assertTrue(len(n9.neighbors) == 2)
+
+        ####
+        self.assertTrue(n1.get_neighbor(east)    is n2)
+        self.assertTrue(n1.get_neighbor(south)   is n4)
+        self.assertTrue(n2.get_neighbor(west)    is n1)
+        self.assertTrue(n2.get_neighbor(south)   is n5)
+        self.assertTrue(n2.get_neighbor(east)    is n3)
+        self.assertTrue(n3.get_neighbor(west)    is n2)
+        self.assertTrue(n3.get_neighbor(south)   is n6)
+        self.assertTrue(n1.get_neighbor(north)   is 0)
+        self.assertTrue(n1.get_neighbor(west)    is 0)
+        self.assertTrue(n2.get_neighbor(north)   is 0)
+        self.assertTrue(n3.get_neighbor(north)   is 0)
+        self.assertTrue(n3.get_neighbor(east)    is 0)
+
+        self.assertTrue(n4.get_neighbor(north)    is n1)
+        self.assertTrue(n4.get_neighbor(east)     is n5)
+        self.assertTrue(n4.get_neighbor(south)    is n7)
+        self.assertTrue(n5.get_neighbor(north)    is n2)
+        self.assertTrue(n5.get_neighbor(east)     is n6)
+        self.assertTrue(n5.get_neighbor(south)    is n8)
+        self.assertTrue(n5.get_neighbor(west)     is n4)
+        self.assertTrue(n6.get_neighbor(north)    is n3)
+        self.assertTrue(n6.get_neighbor(west)     is n5)
+        self.assertTrue(n6.get_neighbor(south)    is n9)
+        self.assertTrue(n4.get_neighbor(west)     is 0)
+        self.assertTrue(n6.get_neighbor(east)     is 0)
+
+        self.assertTrue(n7.get_neighbor(east)    is n8)
+        self.assertTrue(n7.get_neighbor(north)   is n4)
+        self.assertTrue(n8.get_neighbor(west)    is n7)
+        self.assertTrue(n8.get_neighbor(north)   is n5)
+        self.assertTrue(n8.get_neighbor(east)    is n9)
+        self.assertTrue(n9.get_neighbor(west)    is n8)
+        self.assertTrue(n9.get_neighbor(north)   is n6)
+        self.assertTrue(n7.get_neighbor(south)   is 0)
+        self.assertTrue(n7.get_neighbor(west)    is 0)
+        self.assertTrue(n8.get_neighbor(south)   is 0)
+        self.assertTrue(n9.get_neighbor(south)   is 0)
+        self.assertTrue(n9.get_neighbor(east)    is 0)
+
+    def test_add_neighbor(self):
+        g = Graph()
+        n1 = g.add_node()
+        n2 = g.add_node()
+        n3 = g.add_node()
+        n4 = g.add_node()
+        n5 = g.add_node()
+        n6 = g.add_node()
+        n7 = g.add_node()
+        n8 = g.add_node()
+        n9 = g.add_node()
+
+        # Imagine a configuration similar to a telephone numpad
+        north = Direction.NORTH()
+        east  = Direction.EAST()
+        south = Direction.SOUTH()
+        west  = Direction.WEST()
+
+        self.assertTrue(n1.add_neighbor(n2, east))
+        self.assertTrue(n1.add_neighbor(n4, south))
+        self.assertTrue(n2.add_neighbor(n3, east))
+        self.assertTrue(n2.add_neighbor(n5, south))
+        self.assertTrue(n3.add_neighbor(n6, south))
+
+        self.assertTrue(n4.add_neighbor(n5, east))
+        self.assertTrue(n4.add_neighbor(n7, south))
+        self.assertTrue(n5.add_neighbor(n6, east))
+        self.assertTrue(n5.add_neighbor(n8, south))
+        self.assertTrue(n6.add_neighbor(n9, south))
+
+        self.assertTrue(n7.add_neighbor(n8, east))
+        self.assertTrue(n8.add_neighbor(n5, north))
+        self.assertTrue(n8.add_neighbor(n9, east))
+
+        self.assertTrue(len(n1.neighbors) == 2)
+        self.assertTrue(len(n2.neighbors) == 3)
+        self.assertTrue(len(n3.neighbors) == 2)
+        self.assertTrue(len(n4.neighbors) == 3)
+        self.assertTrue(len(n5.neighbors) == 4)
+        self.assertTrue(len(n6.neighbors) == 3)
+        self.assertTrue(len(n7.neighbors) == 2)
+        self.assertTrue(len(n8.neighbors) == 3)
+        self.assertTrue(len(n9.neighbors) == 2)
+
+        ####
+        self.assertTrue(n1.get_neighbor(east)    is n2)
+        self.assertTrue(n1.get_neighbor(south)   is n4)
+        self.assertTrue(n2.get_neighbor(west)    is n1)
+        self.assertTrue(n2.get_neighbor(south)   is n5)
+        self.assertTrue(n2.get_neighbor(east)    is n3)
+        self.assertTrue(n3.get_neighbor(west)    is n2)
+        self.assertTrue(n3.get_neighbor(south)   is n6)
+        self.assertTrue(n1.get_neighbor(north)   is 0)
+        self.assertTrue(n1.get_neighbor(west)    is 0)
+        self.assertTrue(n2.get_neighbor(north)   is 0)
+        self.assertTrue(n3.get_neighbor(north)   is 0)
+        self.assertTrue(n3.get_neighbor(east)    is 0)
+
+        self.assertTrue(n4.get_neighbor(north)    is n1)
+        self.assertTrue(n4.get_neighbor(east)     is n5)
+        self.assertTrue(n4.get_neighbor(south)    is n7)
+        self.assertTrue(n5.get_neighbor(north)    is n2)
+        self.assertTrue(n5.get_neighbor(east)     is n6)
+        self.assertTrue(n5.get_neighbor(south)    is n8)
+        self.assertTrue(n5.get_neighbor(west)     is n4)
+        self.assertTrue(n6.get_neighbor(north)    is n3)
+        self.assertTrue(n6.get_neighbor(west)     is n5)
+        self.assertTrue(n6.get_neighbor(south)    is n9)
+        self.assertTrue(n4.get_neighbor(west)     is 0)
+        self.assertTrue(n6.get_neighbor(east)     is 0)
+
+        self.assertTrue(n7.get_neighbor(east)    is n8)
+        self.assertTrue(n7.get_neighbor(north)   is n4)
+        self.assertTrue(n8.get_neighbor(west)    is n7)
+        self.assertTrue(n8.get_neighbor(north)   is n5)
+        self.assertTrue(n8.get_neighbor(east)    is n9)
+        self.assertTrue(n9.get_neighbor(west)    is n8)
+        self.assertTrue(n9.get_neighbor(north)   is n6)
+        self.assertTrue(n7.get_neighbor(south)   is 0)
+        self.assertTrue(n7.get_neighbor(west)    is 0)
+        self.assertTrue(n8.get_neighbor(south)   is 0)
+        self.assertTrue(n9.get_neighbor(south)   is 0)
+        self.assertTrue(n9.get_neighbor(east)    is 0)
+
+
     # def test_breadth_first_search(self):
     #     pass
 
