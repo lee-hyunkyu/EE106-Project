2e266d66ee26d397e70a879881f518ed2ddc5488
Hyunkyu Michael Lee <hkl0902@gmail.com>
2017-11-29 20:00:44 -0800

    Adds tests for direction

--- tests/direction_tests.py
+++ tests/direction_tests.py
@@ -15,20 +15,32 @@ class TestGraph(unittest.TestCase):
         self.assertTrue(west.dir == Direction.WEST_VAL)
         self.assertTrue(north.dir == Direction.NORTH_VAL)
 
-        self.assertTrue(south == north.opposite())
-        self.assertTrue(north == south.opposite())
-        self.assertTrue(east  == west.opposite())
-        self.assertTrue(west  == east.opposite())
+        self.assertTrue(south == north.opposite_direction())
+        self.assertTrue(north == south.opposite_direction())
+        self.assertTrue(east  == west.opposite_direction())
+        self.assertTrue(west  == east.opposite_direction())
 
-        self.assertFalse(south == west.opposite())
-        self.assertFalse(south == east.opposite())
-        self.assertFalse(north == west.opposite())
-        self.assertFalse(north == east.opposite())
+        self.assertFalse(south == west.opposite_direction())
+        self.assertFalse(south == east.opposite_direction())
+        self.assertFalse(north == west.opposite_direction())
+        self.assertFalse(north == east.opposite_direction())
 
-        self.assertFalse(south.opposite() == west)
-        self.assertFalse(south.opposite() == east)
-        self.assertFalse(north.opposite() == west)
-        self.assertFalse(north.opposite() == east)
+        self.assertFalse(south.opposite_direction() == west)
+        self.assertFalse(south.opposite_direction() == east)
+        self.assertFalse(north.opposite_direction() == west)
+        self.assertFalse(north.opposite_direction() == east)
+
+    def test_clockwise_turn_direction(self):
+        north = Direction.NORTH()
+
+        turned = north.clockwise_turn_direction()
+        self.assertTrue(turned == Direction.EAST())
+        turned = turned.clockwise_turn_direction()
+        self.assertTrue(turned == Direction.SOUTH())
+        turned = turned.clockwise_turn_direction()
+        self.assertTrue(turned == Direction.WEST())
+        turned = turned.clockwise_turn_direction()
+        self.assertTrue(turned == Direction.NORTH())
 
 if __name__ == '__main__':
     unittest.main()
